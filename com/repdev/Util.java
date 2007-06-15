2df6023a164a247ff2fb700a0c303b8cbc739ac5
jake <none@none>
2007-06-15 21:35:13 +0000

    --HG-- branch : RepDev extra : convert_revision : svn%3A20f11d81-ada4-42f0-a65e-a867cb3c61c8/trunk/RepDev%40258

--- com/repdev/Util.java
+++ com/repdev/Util.java
@@ -1,11 +1,41 @@
 package com.repdev;
 
+import java.util.Date;
+
 
 /**
  * Small symitar related utility methods, all static
  * @author poznanja
  *
  */
+
+//TODO: Write these methods!
 public class Util {
- 
+	
+	/**
+	 * Parse Symitar Dates
+	 * Date, ex= 05222006
+	 * Time, ex = 42 = 12:42 AM
+	 *      ex 2312 = 11:12PM
+	 * @param date
+	 * @param time
+	 * @return
+	 */
+	public static Date parseDate( String date, String time){
+		return null;
+	}
+	
+	public static Date parseDate( String date){
+		return parseDate( date, "0" );
+	}
+	
+	/**
+	 * Return nice byte size, ex. 1.24KB for symitar files
+	 * @param size
+	 * @return
+	 */
+	public static String getByteStr(int size){
+		return "" + size;
+	}
+	
 }
