af5531b6b2dbb09697550b5e2340fbcb835074d6
jake <none@none>
2007-06-26 00:38:36 +0000

    Refactoring
    --HG--
    branch : RepDev
    extra : convert_revision : svn%3A20f11d81-ada4-42f0-a65e-a867cb3c61c8/trunk/RepDev%40288

--- com/repdev/TabTextView.java
+++ com/repdev/TabTextView.java
@@ -3,11 +3,12 @@ package com.repdev;
 import org.eclipse.swt.custom.StyledText;
 
 /**
- * Any tab control should implement this interface, so we can do operations on the styled text it uses.
+ * Any tab control in the main window that works with a text view should at least implement this interface, so we can do operations on the styled text it uses.
+ * 
  * @author Jake Poznanski
- * @see TabTextEditorView
+ * @see TabTextEditorView for text EDTIORS, which is this class + aditional stuff
  */
-public interface TabTextView {
+public interface TabTextView extends TabView{
 	public StyledText getStyledText();
 
 }
---
+++ com/repdev/TabView.java
@@ -0,0 +1,11 @@
+package com.repdev;
+
+/**
+ * Blank interface for now, but in the future any tab items in the main window should at least
+ *  implement this interface and provide some generic methods to use with them
+ * @author Jake Poznanski
+ *
+ */
+public interface TabView {
+
+}
