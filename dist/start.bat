5cd07e3b00815e0732d1b33e30ddb0ce032de467
jake <none@none>
2007-08-02 15:28:31 +0000

    Pushing out 0.9.0, basically release candidate for 1.0
    --HG--
    branch : RepDev
    extra : convert_revision : svn%3A20f11d81-ada4-42f0-a65e-a867cb3c61c8/trunk/RepDev%40456

--- dist/start.bat
+++ dist/start.bat
@@ -1,3 +1,3 @@
 svnbin\svn.exe cleanup
 svnbin\svn.exe up
-java -cp swt-win/swt.jar;./ -Djava.library.path=swt-win com.repdev.RepDevMain
+java -cp swt-win/swt.jar;swtcompare.jar;./ -Djava.library.path=swt-win com.repdev.RepDevMain
