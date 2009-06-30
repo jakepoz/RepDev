13f1bcc94feffb337094562aa9082b845eaa4bfc
ryan <none@none>
2009-06-30 15:24:45 -0400

    build.xml tweaks (more exclusions)
    modified launcher to not try and svn update
    
    rebuilt launcher executable (repdev.exe)
    
    generated dist (not included in version control)
    
    --HG--
    branch : current-development

--- com/repdev/launcher/Launcher.java
+++ com/repdev/launcher/Launcher.java
@@ -20,12 +20,9 @@ public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 		boolean log = false;
-		boolean noupdate = false;
 		for(String arg: args) {
 			if( arg.equals("--log") )
 				log = true;
-			if( arg.equals("--no-update") )
-				noupdate = true;
 			if( arg.equals("--help") || arg.equals("-h") ) {
 				usage();
 				System.exit(1);
@@ -45,48 +42,10 @@ public class Launcher {
 			System.err.println(">> Started: " + date + "\n");
 		}
 
-		// Try to update repdev via svn...
-		if( !noupdate ) {
-			try {
-				exec(System.getProperty("user.dir") + "\\svnbin\\svn.exe cleanup");
-				exec(System.getProperty("user.dir") + "\\svnbin\\svn.exe update");	
-				System.out.println();
-				System.err.println();
-			} catch (Exception e) {
-				System.err.println("Could not update via subversion.  This may not be a big issue, but to update you will need to download new zip packages");
-				System.err.println("Error: " + e.getMessage());
-			}
-		}
-
 		// Finally, we launch repdev...
 		com.repdev.RepDevMain.main(args);		
 	}
 
-	/**
-	 * Execute a command and log it to stderr and stdout...
-	 * @param cmd
-	 * @return Exit value of the process.
-	 */
-	private static int exec(String cmd) throws Exception {
-		Process p = Runtime.getRuntime().exec(cmd);
-		BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-		// We want this to finish before dumping output...
-		p.waitFor();	
-
-		String line = "";
-		while( (line = stdout.readLine()) != null )
-			System.out.println(line);
-		while( (line = stderr.readLine()) != null )
-			System.err.println(line);
-
-		stdout.close();
-		stderr.close();
-
-		return p.exitValue();
-	}
-	
 	private static void usage() {
 		String[] usage = {
 				"RepDev Launcher - Usage:",
@@ -94,7 +53,6 @@ public class Launcher {
 				"\tCommand\tAction",
 				"\t--help or -h\tThis message",
 				"\t--log\tLog all output to stdout.txt and stderr.txt",
-				"\t--no-update\tSkip SVN update (faster startup)",
 				"",
 		};
 		
