3264606ce8b766e33f45be778143dfeb85535369
thechillman422 <junkmail92630@yahoo.com>
2014-05-29 08:13:18 -0700

    Added over 2,000 Symitar Fields to db.txt Added a new Style called bright.  This mimics some of the standard IDEs. Removed references to svn in Start.bat.

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
-svnbin\svn.exe cleanup
-svnbin\svn.exe up
-java -cp swt-win/swt.jar;swtcompare.jar;./ -Djava.library.path=swt-win com.repdev.RepDevMain
+@echo off
+
+set JPATH=
+"%JPATH%java.exe" -version
+
+echo.
+
+"%JPATH%java.exe" -cp swt-win/swt.jar;swtcompare.jar;./ -Djava.library.path=swt-win com.repdev.RepDevMain
