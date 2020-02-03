a037801f315cefb7e97afd4f8e8851ac2e0a07f3
thechillman422 <junkmail92630@yahoo.com>
2020-02-02 17:46:59 -0800

    RepDev Release 1.7.0 Automatically cache the SSH Key. Detect the SSH Key changed and stops. Change the Editor background color when logged into the Live SYM  to remind users that they are in the Live Environment. Added a field for the Hostname in the login shell to quickly log  into a different host.

--- com/repdev/RepDevMain.java
+++ com/repdev/RepDevMain.java
@@ -51,8 +51,8 @@ public class RepDevMain {
 
 	public static final boolean DEVELOPER = false; //Set this flag to enable saving passwords, this makes it easy for developers to log in and check stuff quickly after making changes
 	public static final int VMAJOR = 1;
-	public static final int VMINOR = 6;
-	public static final int VFIX   = 14;
+	public static final int VMINOR = 7;
+	public static final int VFIX   = 0;
 	public static final String VSPECIAL = ""; // "special" string for release names, beta, etc
 
 	public static final String VERSION = VMAJOR + "." + VMINOR + (VFIX>0?"."+VFIX:"") + (DEVELOPER ? "-dev" : "") + (!VSPECIAL.equals("")? " " + VSPECIAL : "");
