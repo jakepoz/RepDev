69e99e57dc98f57900dcd12d070d0d1f4f532263
thechillman422 <junkmail92630@yahoo.com>
2023-11-18 17:57:50 -0800

    RepDev Release 1.7.3 New Features: Save source control version before remote version - This will save the RepGen to source control before saving the file to Symitar.  Just in case there is an issue with Symitar, at least the RepGen is saved locally.  By Peter Vinson.
    Quick compare local file to production file - This will allow users
    to right click on a local RepGen/LetterFile/HelpFile and compare it
    to the production version.
    
    SYM Description - I can never remember which is the Month End SYM,
    Quarter End SYM etc.  This will allow you to give the SYM a 25
    character description.  Right click on the SYM and select SYM
    Description.  From there, you can set or clear the description.
    
    Remember server - Currently, when you log into a SYM, it always
    pull the server from Options.  RepDev will now remember the last
    server used for that SYM and default to it the next time you log
    in.  When adding a new SYM, RepDev will still pull the server from
    Options.
    
    Update:
    db.txt - Updated for Symitar Release 2023.00
    
    Bug Fix:
    Server in RepDev Title – This  has been a long standing issue where
    the RepDev Window Title always show the Server configured in Options
    (Assuming that you select Show Server in Title).  It will now
    display the Server based on the File selected in the Editor Tabs.

--- com/repdev/RepDevMain.java
+++ com/repdev/RepDevMain.java
@@ -54,7 +54,7 @@ public class RepDevMain {
 	public static final boolean DEVELOPER = false; //Set this flag to enable saving passwords, this makes it easy for developers to log in and check stuff quickly after making changes
 	public static final int VMAJOR = 1;
 	public static final int VMINOR = 7;
-	public static final int VFIX   = 2;
+	public static final int VFIX   = 3;
 	public static final String VSPECIAL = ""; // "special" string for release names, beta, etc
 
 	public static final String VERSION = VMAJOR + "." + VMINOR + (VFIX>0?"."+VFIX:"") + (DEVELOPER ? "-dev" : "") + (!VSPECIAL.equals("")? " " + VSPECIAL : "");
